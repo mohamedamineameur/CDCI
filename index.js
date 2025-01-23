0fbb26ec8cd3f4877be3c13cbf1c626940b73987
aminefaw89 <aminefaw89@gmail.com>
2025-01-23 11:08:27 -0500

    adding web page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -7,10 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    }
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir les fichiers statiques (HTML, CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route principale pour envoyer la page HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
